80d38ffe81adc61bef797045f0b6f93787603eb6
pnochisaki <pnochisaki@gmail.com>
2022-01-08 19:22:57 -0800

    deploy

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Mombre" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          image and text here
         </p>
       </main>
 
